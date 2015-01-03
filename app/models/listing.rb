b799ea6a8b3c801bb17f6b67d44c8874a72dcaee
Dawood Qureshi <dawood207@hotmail.com>
2015-01-03 16:33:38 +0100

    first commit

---
+++ app/models/listing.rb
@@ -0,0 +1,10 @@
+class Listing < ActiveRecord::Base
+
+
+	has_attached_file :image, :styles => { :medium => "250x>", :thumb => "100x100>" }, :default_url => "noimage.jpg"
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
+
+
+end
